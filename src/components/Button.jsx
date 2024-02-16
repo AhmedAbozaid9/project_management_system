21766152c57d76c31692a705b494f7158bccc97b
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-02-16 11:28:05 +0200

    adds the ability to open the portfolio

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-
-const Button = ({ title, isPrimary }) => {
+const Button = ({ title, isPrimary, action }) => {
   return (
     <button
+      onClick={action}
       className={`text-sm font-medium px-6 py-2 rounded-md ${
         isPrimary ? "bg-primary-purple" : "border-2"
       }`}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+"use client";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
 import Button from "./Button";
 
 const Header = () => {
+  const openPortfolio = () => {
+    window.open("https://simple-portfolio-six-psi.vercel.app/", "_blank");
+  };
+
+  const addProject = () => {
+    console.log("aaa");
+  };
+
   return (
-    <header className="flex gap-5 w-full p-6">
-      <Button title={"Add new project"} />
-      <Button title={"Visit portfolio"} isPrimary={false} />
+    <header className="flex gap-3 w-full p-6">
+      <Button title={"Add new project"} action={addProject} />
+      <Button
+        title={"Visit portfolio"}
+        isPrimary={false}
+        action={openPortfolio}
+      />
       <FaUserCircle size={26} className="ml-auto" />
     </header>
   );
