0da747edfa33b11f218114ccc7328c6a15663348
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-02-21 14:57:54 +0200

    adds the tables to the tables route

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,12 @@
+import ProjectsTable from "@/components/Tables/ProjectsTable";
 import React from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div className="w-full">
+      <ProjectsTable />
+    </div>
+  );
 };
 
 export default index;
