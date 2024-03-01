5c1724f7eff60881877d10f737529e2f00d2a376
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-01 22:17:09 +0200

    fixes the timer layout

--- src/app/timer/page.js
+++ src/app/timer/page.js
@@ -5,11 +5,11 @@ import Timer from "@/components/timer/Timer";
 import TimerStatistics from "@/components/timer/TimerStatistics";
 
 const Page = () => {
-  const [timerStamp, setTimerStamp] = useState(10000);
+  const [timerStamp, setTimerStamp] = useState(1);
 
   return (
     <section className="w-full p-6 flex justify-between items-center">
-      <Timer expiryTimestamp={new Date(Date.now() + timerStamp)} />
+      <Timer timerStamp={timerStamp} setTimerStamp={setTimerStamp} />
       <TimerStatistics />
     </section>
   );
--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -2,66 +2,45 @@ import React, { useState } from "react";
 import { useTimer } from "react-timer-hook";
 import { Button } from "@nextui-org/react";
 
-const Timer = ({ expiryTimestamp }) => {
-  const [chosenTime, setChosenTime] = useState({
-    seconds: 0,
-    minutes: 0,
-    hours: 0,
-  });
-  console.log(chosenTime);
+const Timer = ({ timerStamp, setTimerStamp }) => {
   const {
     totalSeconds,
     seconds,
     minutes,
-    hours,
-    days,
     isRunning,
     start,
     pause,
     resume,
     restart,
   } = useTimer({
-    expiryTimestamp,
+    expiryTimestamp: new Date(Date.now() + 1000 * 60 * timerStamp),
     autoStart: false,
     onExpire: () => console.warn("onExpire called"),
   });
 
-  const changeTime = (unit) => {
-    const name = unit.name;
-    setChosenTime((prev) => ({ ...prev, [name]: unit.value }));
+  const handleTimerOnChange = (e) => {
+    if (e.target.value > 180) setTimerStamp(180);
+    else if (e.target.value < 1) setTimerStamp(1);
+    else setTimerStamp(e.target.value);
   };
 
+  console.log(totalSeconds);
+  console.log(isRunning);
   return (
     <div className="flex flex-col flex-1 justify-center items-center">
-      <div className="w-72 h-72 rounded-full border-2 border-white flex items-center justify-center text-5xl font-medium">
+      <div className="w-72 h-72 gap-2 p-3 rounded-full border-2 border-white flex items-center justify-center flex-col text-5xl font-medium">
         <input
-          value={chosenTime.hours}
-          onChange={(e) => changeTime(e.target)}
+          value={timerStamp}
+          onChange={handleTimerOnChange}
           type="number"
-          name="hours"
-          className="bg-transparent flex-1"
-          min="0"
+          className="bg-transparent text-center w-1/2"
+          min="1"
+          max="180"
         />
-        <input
-          value={chosenTime.minutes}
-          onChange={(e) => changeTime(e.target)}
-          type="number"
-          name="minutes"
-          className="bg-transparent flex-1"
-          min="0"
-        />
-        <span>:</span>
-        <input
-          value={chosenTime.seconds}
-          onChange={(e) => changeTime(e.target)}
-          type="number"
-          name="seconds"
-          className="flex-1"
-          min="0"
-        />
+        <span className="text-lg font-medium">Mins</span>
       </div>
       <Button
-        className="bg-primary-purple font-medium max-w-28 my-6  "
+        className="bg-primary-purple font-medium w-48 my-6  "
         onPress={isRunning ? pause : start}
       >
         {isRunning ? "Pause" : "Start"}
