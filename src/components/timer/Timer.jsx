b3e6aee873e26173c668e0591c0907f91241a8de
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-02 19:06:49 +0200

    fixes the timer buttons layout

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -3,20 +3,15 @@ import { useTimer } from "react-timer-hook";
 import { Button } from "@nextui-org/react";
 
 const Timer = ({ timerStamp, setTimerStamp }) => {
-  const {
-    totalSeconds,
-    seconds,
-    minutes,
-    isRunning,
-    start,
-    pause,
-    resume,
-    restart,
-  } = useTimer({
-    expiryTimestamp: new Date(Date.now() + 1000 * 60 * timerStamp),
-    autoStart: false,
-    onExpire: () => console.warn("onExpire called"),
-  });
+  const [isOver, setIsOver] = useState(true);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const { seconds, minutes, isRunning, start, pause, resume, restart } =
+    useTimer({
+      expiryTimestamp: new Date(Date.now() + 1000 * 60 * timerStamp),
+      autoStart: false,
+      onExpire: () => endTimer(),
+    });
 
   const handleTimerOnChange = (e) => {
     if (e.target.value > 180) setTimerStamp(180);
@@ -24,27 +19,65 @@ const Timer = ({ timerStamp, setTimerStamp }) => {
     else setTimerStamp(e.target.value);
   };
 
-  console.log(totalSeconds);
-  console.log(isRunning);
+  const startTimer = () => {
+    setIsOver(false);
+    if (isPaused) {
+      setIsPaused(false);
+      resume();
+    } else {
+      start();
+    }
+    console.log(timerStamp);
+  };
+
+  const pauseTimer = () => {
+    setIsPaused(true);
+    pause();
+  };
+
+  const endTimer = () => {
+    setIsOver(true);
+    restart();
+  };
+
   return (
     <div className="flex flex-col flex-1 justify-center items-center">
-      <div className="w-72 h-72 gap-2 p-3 rounded-full border-2 border-white flex items-center justify-center flex-col text-5xl font-medium">
-        <input
-          value={timerStamp}
-          onChange={handleTimerOnChange}
-          type="number"
-          className="bg-transparent text-center w-1/2"
-          min="1"
-          max="180"
-        />
-        <span className="text-lg font-medium">Mins</span>
+      <div className="text-5xl w-72 h-72 gap-2 p-3 rounded-full border-2 border-white flex items-center justify-center flex-col font-medium">
+        {isOver ? (
+          <>
+            <input
+              value={timerStamp}
+              onChange={handleTimerOnChange}
+              type="number"
+              className="bg-transparent text-center w-1/2"
+              min="1"
+              max="180"
+            />
+            <span className="text-lg font-medium text-gray-300">Minutes</span>
+          </>
+        ) : (
+          <p>
+            {minutes}:{seconds}
+          </p>
+        )}
       </div>
-      <Button
-        className="bg-primary-purple font-medium w-48 my-6  "
-        onPress={isRunning ? pause : start}
-      >
-        {isRunning ? "Pause" : "Start"}
-      </Button>
+      <div className="flex gap-3 items-center justify-center">
+        <Button
+          className="bg-primary-purple font-medium my-6 w-32"
+          onPress={isRunning ? pauseTimer : startTimer}
+        >
+          {isRunning ? "Pause" : isPaused ? "Resume" : "Start"}
+        </Button>
+        {isRunning && (
+          <Button
+            variant="bordered"
+            className="font-medium my-6 w-32"
+            onPress={restart}
+          >
+            End
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
