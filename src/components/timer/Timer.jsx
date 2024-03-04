9741d37196bc801d9a535f3af52c264495069979
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-04 20:04:53 +0200

    changes the timer ui

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -3,20 +3,25 @@ import { Button } from "@nextui-org/react";
 import { useTimer } from "@/hooks/useTimer";
 
 const Timer = () => {
+  const [inputTime, setInputTime] = useState(1);
   const { time, isRunning, isPaused, isExpired, start, pause, end } = useTimer(
-    10,
+    inputTime * 60,
     () => console.log("it is done")
   );
-  console.log(time);
-  console.log("is paused", isPaused);
-  console.log("is expired", isExpired);
 
+  const handleInputChange = (value) => {
+    if (value > 180) setInputTime(180);
+    else if (value < 1) setInputTime(1);
+    else setInputTime(value);
+  };
   return (
     <div className="flex flex-col flex-1 justify-center items-center">
       <div className="text-5xl w-72 h-72 gap-2 p-3 rounded-full border-2 border-white flex items-center justify-center flex-col font-medium">
         <input
+          value={inputTime}
+          onChange={(e) => handleInputChange(e.target.value)}
           type="number"
-          className="bg-transparent text-center w-1/2"
+          className="bg-transparent text-center w-1/2 outline-none"
           min="1"
           max="180"
         />
