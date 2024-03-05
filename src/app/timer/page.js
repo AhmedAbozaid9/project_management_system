766695d7448942443a948c5a304c6c5b452c3340
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-05 14:31:47 +0200

    adds a timer type tabs

--- src/app/timer/page.js
+++ src/app/timer/page.js
@@ -1,15 +1,32 @@
 "use client";
 
 import React, { useState } from "react";
+import { Tabs, Tab } from "@nextui-org/react";
+
 import Timer from "@/components/timer/Timer";
 import TimerStatistics from "@/components/timer/TimerStatistics";
 
 const Page = () => {
-  const [timerStamp, setTimerStamp] = useState(1);
-
+  const [selectedType, setSelectedType] = useState("Timer");
+  console.log(selectedType);
   return (
     <section className="w-full p-6 flex justify-between items-center">
-      <Timer timerStamp={timerStamp} setTimerStamp={setTimerStamp} />
+      <div className="flex flex-col gap-5 justify-center">
+        <Tabs
+          size="sm"
+          selectedKey={selectedType}
+          onSelectionChange={setSelectedType}
+          radius="full"
+          className="mx-auto"
+          classNames={{
+            panel: "bg-primary-purple",
+          }}
+        >
+          <Tab key="Timer" title="Timer" />
+          <Tab key="Stopwatch" title="Stopwatch" />
+        </Tabs>
+        <Timer />
+      </div>
       <TimerStatistics />
     </section>
   );
