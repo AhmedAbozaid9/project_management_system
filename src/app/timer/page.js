41621d6ee4a8709638479d7e0f163a9379b48b72
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-05 14:36:25 +0200

    adds a basic select for the projects timer

--- src/app/timer/page.js
+++ src/app/timer/page.js
@@ -1,10 +1,15 @@
 "use client";
 
 import React, { useState } from "react";
-import { Tabs, Tab } from "@nextui-org/react";
+import { Tabs, Tab, Select, SelectItem } from "@nextui-org/react";
 
 import Timer from "@/components/timer/Timer";
 import TimerStatistics from "@/components/timer/TimerStatistics";
+
+const projects = [
+  { key: 1, title: "project 1" },
+  { key: 2, title: "project 2" },
+];
 
 const Page = () => {
   const [selectedType, setSelectedType] = useState("Timer");
@@ -12,6 +17,14 @@ const Page = () => {
   return (
     <section className="w-full p-6 flex justify-between items-center">
       <div className="flex flex-col gap-5 justify-center">
+        <Select size="sm" variant="underlined" label="Select a project">
+          {projects.map((project) => (
+            <SelectItem key={project.key} value={project.title}>
+              {project.title}
+            </SelectItem>
+          ))}
+        </Select>
+
         <Tabs
           size="sm"
           selectedKey={selectedType}
