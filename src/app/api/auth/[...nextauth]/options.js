ec451e79b776fbe8ffcdf01396735d2e9c31c6fc
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-06 22:05:11 +0200

    adds the database

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -4,33 +4,10 @@ import GoogleProvider from "next-auth/providers/google";
 export const options = {
   providers: [
     GitHubProvider({
-      profile(profile) {
-        console.log("Profile GitHub: ", profile);
-
-        let userRole = "GitHub User";
-        if (profile?.email == "ahmedabozaid493@gmail.com") {
-          userRole = "admin";
-        }
-
-        return {
-          ...profile,
-          role: userRole,
-        };
-      },
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_Secret,
     }),
     GoogleProvider({
-      profile(profile) {
-        console.log("Profile Google: ", profile);
-
-        let userRole = "Google User";
-        return {
-          ...profile,
-          id: profile.sub,
-          role: userRole,
-        };
-      },
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_Secret,
     }),
@@ -45,4 +22,6 @@ export const options = {
       return session;
     },
   },
+  signIn: {},
+  secret: process.env.NEXTAUTH_SECRET,
 };
---
+++ src/utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("MongoDb is already connected");
+    return;
+  }
+  try {
+    console.log(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "flair_fusion",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("mongoDB connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
