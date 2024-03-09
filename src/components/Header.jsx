a9f5334dabde4f8f763bc41cdc145f67195537e5
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-09 19:40:45 +0200

    adds the timer to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 import React, { useContext } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
-import { Avatar, Button } from "@nextui-org/react";
+import { Avatar, Button, Chip } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 
 import Modal from "./addProject/Modal";
@@ -12,7 +12,7 @@ import { TimerContext } from "@/contexts/timerContext";
 const Header = () => {
   const { data: session } = useSession();
   const { timer } = useContext(TimerContext);
-  console.log(timer);
+  console.log(!!timer);
 
   const openPortfolio = () => {
     window.open("https://simple-portfolio-six-psi.vercel.app/", "_blank");
@@ -39,12 +39,15 @@ const Header = () => {
       >
         <FaExternalLinkAlt className="sm:hidden " onClick={openPortfolio} />
       </Button>
-      <Avatar
-        size="sm"
-        className="ml-auto"
-        name={session?.user.name}
-        src={session?.user.image}
-      />
+      <div className="ml-auto flex items-center gap-3">
+        {!!timer && (
+          <Chip color="success">
+            {Math.floor(timer / 60) ? Math.floor(timer / 60) : "00"}:
+            {timer % 60 ? timer % 60 : "00"}
+          </Chip>
+        )}
+        <Avatar size="sm" name={session?.user.name} src={session?.user.image} />
+      </div>
     </header>
   );
 };
