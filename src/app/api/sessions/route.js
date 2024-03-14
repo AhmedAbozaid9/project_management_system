621f579cbd34694e677c5b6772a9a617638e907a
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-03-14 07:11:53 +0200

    adds pagination to the sessions endpoint

--- src/app/api/sessions/route.js
+++ src/app/api/sessions/route.js
@@ -1,10 +1,20 @@
 import { connectToDB } from "@/utils/database";
 import Session from "@/models/session";
 
-export const GET = async () => {
+const sessionsPerPage = 20;
+
+export const GET = async (req) => {
+  const url = new URL(req.url);
+  const searchParams = new URLSearchParams(url.search);
+  const page = parseInt(searchParams.get("page")) || 1;
   try {
     await connectToDB();
-    const sessions = await Session.find({});
+    const startIndex = (page - 1) * sessionsPerPage;
+
+    const sessions = await Session.find({})
+      .skip(startIndex)
+      .limit(sessionsPerPage);
+
     return new Response(JSON.stringify(sessions), { status: 200 });
   } catch (e) {
     console.log(e);
