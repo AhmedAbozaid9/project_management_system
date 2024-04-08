8091ede3495996fe4920c4b6f64a7ee74594474b
Ahmed Abozaid <--global>
2024-04-08 02:43:11 +0200

    gets each project data in the front end

--- src/app/projects/[projectId]/page.js
+++ src/app/projects/[projectId]/page.js
@@ -1,9 +1,24 @@
-import React from "react";
+"use client";
 
-const page = ({ params }) => {
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Project = ({ params }) => {
   const projectId = params.projectId;
+  const [project, setProject] = useState({});
 
-  return <div>{projectId}</div>;
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(`/api/projects/${projectId}`);
+      setProject(data);
+    })();
+  }, [projectId]);
+
+  return (
+    <div className="flex flex-col w-full">
+      <h1 className="text-left flex-1">{project.title}</h1>
+    </div>
+  );
 };
 
-export default page;
+export default Project;
